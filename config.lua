4e852061ead73a7a09ea94af89deaeae3e2b0c2d
Peng Li <jerryli1981@gmail.com>
2016-02-09 19:17:44 -0600

    enhance config

--- config.lua
+++ config.lua
@@ -11,24 +11,26 @@ config = {}
 local alphabet = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
 
 seq_length = 100
+att_name = "type"
+att_num_label = 4
 
 -- Training data
 config.train_data = {}
-config.train_data.file = paths.cwd() .. "/data/type_train.t7b"
+config.train_data.file = paths.cwd() .. "/data/".. att_name .. "_train.t7b"
 config.train_data.alphabet = alphabet
 config.train_data.length = seq_length
 config.train_data.batch_size = 128
 
 -- Validation data
 config.val_data = {}
-config.val_data.file =  paths.cwd() .. "/data/type_dev.t7b"
+config.val_data.file =  paths.cwd() .. "/data/".. att_name .. "_dev.t7b"
 config.val_data.alphabet = alphabet
 config.val_data.length = seq_length
 config.val_data.batch_size = 128
 
 -- Test data
 config.test_data = {}
-config.test_data.file =  paths.cwd() .. "/data/type_test.t7b"
+config.test_data.file =  paths.cwd() .. "/data/".. att_name .. "_test.t7b"
 config.test_data.alphabet = alphabet
 config.test_data.length = seq_length
 config.test_data.batch_size = 128
@@ -67,7 +69,7 @@ config.model[20] = {module = "nn.Linear", inputSize = 256, outputSize = 256}
 config.model[21] = {module = "nn.Threshold"}
 config.model[22] = {module = "nn.Dropout", p = 0.5}
 -- 1024
-config.model[23] = {module = "nn.Linear", inputSize = 256, outputSize = 4}
+config.model[23] = {module = "nn.Linear", inputSize = 256, outputSize = att_num_label}
 config.model[24] = {module = "nn.LogSoftMax"}
 
 -- The loss
@@ -91,7 +93,7 @@ config.main.eras = 1
 config.main.epoches = 2
 config.main.randomize = 5e-2
 config.main.dropout = true
-config.main.save = paths.cwd() .. "/models"
+config.main.save = paths.cwd() .. "/models/" ..att_name
 config.main.collectgarbage = 100
 config.main.logtime = 5
 config.main.validate = true
